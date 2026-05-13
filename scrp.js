d460d6c840c3c67a5f54d20226893f6be4563595
burdaanastasiia <ivan.b.burda@gmail.com>
2026-05-13 21:25:02 +0300

    Add files via upload

---
+++ scrp.js
@@ -0,0 +1,25 @@
+const images = ["img1.jpg", "img2.jpg", "img3.webp", "img4.webp"];
+let currentIndex = 0;
+
+const n = images.length
+const mainImg = document.getElementById('mainImg');
+const nxtBtn = document.getElementById('nxtBtn');
+const prvBtn = document.getElementById('prvBtn');
+
+
+function updateImage() {
+  mainImg.src = images[currentIndex];
+  console.log("Loading path:" + mainImg.src)
+}
+
+nxtBtn.onclick = function() {
+    currentIndex = (currentIndex + 1) % n;
+    updateImage();
+}
+
+prvBtn.onclick = function() {
+    currentIndex = (currentIndex - 1 + n) % n;
+    updateImage();
+}
+
+updateImage();
